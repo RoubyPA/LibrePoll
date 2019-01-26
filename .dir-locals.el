647303a13926ddf320d1cd8f7c7a6ffe09567c80
Pierre-Antoine Rouby <contact@parouby.fr>
2019-01-26 18:51:07 +0100

    librepoll: Add emacs dir config.
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((nil . ((fill-column . 78)
+         (tab-width   .  8)))
+ (scheme-mode . ((indent-tabs-mode . nil)
+                 (eval
+                  .
+                  (setq scheme-font-lock-keywords-2
+                        (cons
+                         '("(\\(define-sql-request\\|define-handler-matcher\\)" .
+                           ((1 font-lock-keyword-face)))
+                         scheme-font-lock-keywords-2)))))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil))))
